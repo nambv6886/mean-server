c5e650453212385b591bf64635e7922c6c9b11f4
nam bui van <nambv6886@gmail.com>
2020-02-10 17:10:11 +0700

    add id in hero model

--- models/hero.js
+++ models/hero.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const HeroSchema = mongoose.Schema({
+  id: Number,
   name: String,
   heroClass: String,
   attack: Number,
--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use('/images', express.static(path.join('public/images')));
 app.use(cors());
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 app.use('/api/user', userRoute);
 app.use('/api/hero', heroRoute);
